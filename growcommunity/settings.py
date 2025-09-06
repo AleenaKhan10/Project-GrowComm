e6b95cf1740bd94d38233cc2abbdf8b27daa66a7
AleenaKhan10 <aleenakhanraees40@gmail.com>
2025-09-05 21:10:01 -0700

    Update ALLOWED_HOSTS in settings.py to include specific IP addresses and localhost for improved security and accessibility during development.

--- growcommunity/settings.py
+++ growcommunity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h_%#ss$#i$peguhau6-nk!$xrg_7%0&*60fu1ewtqfoj=r%aq(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.20.31.158', 'localhost', '127.0.0.1']
 
 
 # Application definition
